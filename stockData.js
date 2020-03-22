5f87fdb336d86b0cb6fa9b6a8839109d9a64cd21
Joel Hamilton <joelhamilton5@gmail.com>
2020-03-22 15:36:53 -0400

    fetch data by header name

--- stockData.js
+++ stockData.js
@@ -49,8 +49,7 @@ function getHeaders(sheet) {
 function setRowValues(sheet, rowNum, info) {
   let headers = getHeaders(sheet);
   headers.forEach((header, index) => {
-      Logger.log(index);
-    if (info[header]) {
+    if (Object.keys(info).includes(header)) {
       var cell = numToSSColumn(index) + rowNum;
       sheet.getRange(cell).setValue(info[header]);
     }
